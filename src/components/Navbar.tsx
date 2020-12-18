2e4b1dc5cd1b354cdf08450fac6226317d53c428
Adam <adammuman81@gmail.com>
2020-12-18 09:06:17 +0000

    [ADD] image :tada:

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,47 @@
-import { Box, Flex, Container } from "@chakra-ui/react";
-import React from "react";
-function Navbar() {
+import { Box, Flex, Image, Link } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+
+interface MenuItemsProps {
+  children: ReactNode;
+}
+const MenuItems = ({ children }: MenuItemsProps) => (
+  <Link mt={{ base: 4, md: 0 }} mr={6} display="block">
+    {children}
+  </Link>
+);
+const Navbar = () => {
   return (
     <Flex
       as="nav"
-      wrap="wrap"
       align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
       bg="white"
-      //   justify="space-evenly"
-      padding="2.0rem"
-      color="black"
-      shadow="14px 14px 40px 0 rgba(118, 126, 173, 0.08)"
+      color="teal.300"
+      borderBottom="1px solid black"
     >
-      <Box mr="4rem"></Box>
-      <Box color="green">test</Box>
+      <Flex align="center" mr={5}>
+        {/* <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
+          Chakra UI & React
+        </Heading> */}
+        <Image src="https://uploads-ssl.webflow.com/5f96d37adfe170593d658cbf/5f96e016c07b280c33adf0ef_otter-logo.png" />
+      </Flex>
+
+      <Box
+        display="flex"
+        width="auto"
+        alignItems="center"
+        flexGrow={1}
+        color="teal.300"
+      >
+        <MenuItems>Home</MenuItems>
+        <MenuItems>Blogs</MenuItems>
+        <MenuItems>About</MenuItems>
+        <MenuItems>Contact</MenuItems>
+      </Box>
     </Flex>
   );
-}
+};
 
 export default Navbar;
